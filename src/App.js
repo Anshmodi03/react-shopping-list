428c434494f5e3931d4ae48253477ecb53fb7dc8
Ansh Modi <160162966+Anshmodi03@users.noreply.github.com>
2024-08-24 22:12:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import Form from "./components/Form";
+import { useState } from "react";
+import ItemList from "./components/ItemList";
+import Filter from "./components/Filter";
+import logo from "./images/note.png";
+
+function App() {
+  const [items, setItems] = useState([
+    "Apples",
+    "Orange Juice",
+    "Oreos",
+    "Milk",
+  ]);
+
+  const [filter, setFilter] = useState("");
+
+  //Adds a new item to the list
+  const handleAdd = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
+  //Removes an item from the list based on its index
+  const handleDelete = (index) => {
+    setItems(items.filter((_, i) => i !== index));
+  };
+
+  //contains items that match the filter text
+  const filteredItems = items.filter((item) =>
+    item.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  //Clears all items from the list
+  const handleClearAll = () => {
+    setItems([]);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <img src={logo} alt=""></img>
+        <h1>Shopping List</h1>
+      </header>
+      <Form onAdd={handleAdd} />
+      <Filter filter={filter} onFilterChange={setFilter} />
+      <ItemList items={filteredItems} onDelete={handleDelete} />
+      <button id="clear" className="btn-clear" onClick={handleClearAll}>
+        Clear All
+      </button>
+    </div>
+  );
+}
+
+export default App;
